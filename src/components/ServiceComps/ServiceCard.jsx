4fc6c83770984152aa26526b342edfd217acb613
MaestroOG <muneeburrehman822@gmail.com>
2024-09-23 20:29:42 +0500

    update  service-card

--- src/components/ServiceComps/ServiceCard.jsx
+++ src/components/ServiceComps/ServiceCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { assets } from '../../assets/assets';
 import { useNavigate } from 'react-router-dom'
 
@@ -18,15 +18,17 @@ const ServiceCard = () => {
     return (
         <div className='my-8'>
             {/* Left */}
-            <div className='bg-[#0AB247] clipped w-[179px] h-[158px] flex flex-col justify-center absolute left-4 px-3 rounded-tl-3xl rounded-bl-3xl'>
+            <div className='bg-[#0AB247] clipped w-[179px] h-[158px] flex flex-col justify-center absolute left-[3%] px-3 rounded-tl-3xl rounded-bl-3xl'>
                 <p className='text-[#FFE5A4] font-medium text-base'>Get the best food restaurant and fast delivery with Feres</p>
                 <button className='bg-white text-[#0AB247] rounded-[30px] p-[10px] w-[77px] mt-3' onClick={() => navigate('/allrestaurants')}>View all</button>
             </div>
 
             {/* Right */}
 
-            <img src={cardImgSrc[index]} alt="" className='absolute right-4 rounded-tr-3xl rounded-br-3xl' />
-            <img src={assets.feres_logo} className="absolute right-[7%] top-[21%]" />
+            <div className='relative'>
+                <img src={cardImgSrc[index]} alt="" className='absolute right-[4%] rounded-tr-3xl rounded-br-3xl transition-all ease-in-out' />
+                <img src={assets.feres_logo} className="absolute right-[6%] top-4" />
+            </div>
 
         </div>
     )
