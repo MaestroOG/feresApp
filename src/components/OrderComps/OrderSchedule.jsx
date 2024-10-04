ffc60f027eaea17a9d28ed538c160314111f9858
MaestroOG <muneeburrehman822@gmail.com>
2024-10-05 00:06:32 +0500

    qa changes

--- src/components/OrderComps/OrderSchedule.jsx
+++ src/components/OrderComps/OrderSchedule.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React, { useState, useRef } from 'react';
 import { assets } from '../../assets/assets'
 
 const OrderSchedule = () => {
+    const dateInputRef = useRef(null);
+    const timeInputRef = useRef(null);
+
+    const handleDateLabelClick = () => {
+        // Trigger click event on the hidden time input
+        dateInputRef.current.click();
+    };
+
+    const handleTimeLabelClick = () => {
+        // Trigger click event on the hidden time input
+        dateInputRef.current.click();
+    };
+
     return (
         <div className='px-4 mt-5 bg-white rounded-tl-3xl rounded-tr-3xl flex flex-col gap-5'>
             <div className='flex items-center justify-between'>
                 <div className='flex items-center gap-2'>
                     <img src={assets.calendar_03} alt="" />
-                    <p className='text-base text-[#2F2F3F]'>Schedule order</p>
-                    <input type="time" name="" id="" />
+                    <label htmlFor='date' onClick={handleDateLabelClick} className='text-base text-[#2F2F3F]'>Schedule order</label>
+                    <input type="date" name="" id="date" ref={dateInputRef} className='absolute left-[-9999px]' />
                 </div>
                 <img src={assets.arrow_right} alt="" />
             </div>
@@ -16,7 +29,8 @@ const OrderSchedule = () => {
             <div className='flex items-center justify-between'>
                 <div className='flex items-center gap-2'>
                     <img src={assets.clock_light_green} alt="" />
-                    <p className='text-base text-[#2F2F3F]'>30 min</p>
+                    <label htmlFor='time' onClick={handleTimeLabelClick} className='text-base text-[#2F2F3F]'>30 min</label>
+                    <input type="time" name="" id="time" ref={timeInputRef} className='absolute left-[-9999px]' />
                 </div>
                 <img src={assets.arrow_right} alt="" />
             </div>
